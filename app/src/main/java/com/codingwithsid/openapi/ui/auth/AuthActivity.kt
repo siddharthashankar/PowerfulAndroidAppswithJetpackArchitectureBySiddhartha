611d586bd793c818692a8731361799bd14e23e54
SiddharthaShankar84 <shankar.siddhartha84@gmail.com>
2021-01-10 01:32:22 +0800

    Initial commit on 10 Jan 2021

---
+++ app/src/main/java/com/codingwithsid/openapi/ui/auth/AuthActivity.kt
@@ -0,0 +1,12 @@
+package com.codingwithsid.openapi.ui.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.codingwithsid.openapi.R
+
+class AuthActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_auth)
+    }
+}
